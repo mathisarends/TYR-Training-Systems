bb7d33c95ab5d8a46a1aab354a6dc5d81a1b531d
mathisarends <140075923+mathisarends@users.noreply.github.com>
2024-02-19 14:46:32 +0100

    Added edit training plan functionality

--- routes/training.ts
+++ routes/training.ts
@@ -2,9 +2,12 @@ import express from "express";
 const router = express.Router();
 
 import { UserInterface } from "../src/models/user.types.js";
+
 import TrainingBlock from "../src/models/trainingBlock.model.js";
+import { TrainingBlockInterface } from "../src/models/trainingBlock.types.js";
 
 import { checkAuthenticated } from "../middleware/authMiddleware.js";
+import { formatCustomDate } from "../src/public/generic/formatDate.js";
 
 router.get("/", checkAuthenticated, (req, res) => {
 
@@ -13,13 +16,84 @@ router.get("/", checkAuthenticated, (req, res) => {
     const username = user.name;
     const trainingPlans = user.customPlans;
 
-
     res.render("training/index", {
         defaultLayout: true,
         carousel: false,
         username,
-        trainingPlans
+        trainingPlans,
+        formatCustomDate,
     })
+})
+
+router.delete("/delete/:id", checkAuthenticated, async (req, res) => {
+
+    try {
+        const user = res.locals.user;
+    
+        const deleteId = parseInt(req.params.id);
+        user.customPlans?.splice(deleteId, 1);
+    
+        await user.save();
+
+        res.status(200).json({});
+
+    } catch (error) {
+        const message = "Es ist ein Fehler beim löschen des Trainingsplans aufgetreten " + error;
+        console.log(message);
+        res.status(500).json({ message: message });
+    }
+})
+
+router.get("/planInfo/:planIndex", checkAuthenticated, async (req, res) => {
+    try {
+        const user = res.locals.user;
+        const planIndex = parseInt(req.params.planIndex);
+
+        console.log(planIndex);
+
+        if (planIndex >= 0 && planIndex < user.customPlans.length) {
+            const trainingPlan = user.customPlans[planIndex];
+
+            const jsonTrainingPlan = trainingPlan.toObject();
+
+            res.json(jsonTrainingPlan);
+        } else {
+            res.status(404).json({ error: "Plan nicht gefunden" });
+        }
+    } catch (error) {
+        res.status(500).json({ error: "Interner Serverfehler" });
+    }
+});
+
+router.patch("/plan/:planIndex", checkAuthenticated, async (req, res) => {
+    try {
+        const user = res.locals.user;
+
+        const planIndex = parseInt(req.params.planIndex);
+
+        console.log("we here and right");
+        console.log(planIndex);
+
+        if (planIndex >= 0 && planIndex < user.customPlans.length) { 
+            const trainingPlan = user.customPlans[planIndex];
+
+            trainingPlan.title = req.body.title;
+            trainingPlan.trainingFrequency = req.body.trainingFrequency;
+            trainingPlan.trainingPhase = req.body.trainingPhase;
+            trainingPlan.weightRecommandationBase = req.body.weightPlaceholders;
+    
+            await user.save();
+    
+            res.status(200).json({});
+        } else {
+            res.status(404).json({ error: "Plan nicht gefunden" });
+        }
+    } catch (error) {
+        const msg = "Es ist ein Fehler beim bearbeiten des Plans aufgetreten " + error;
+        console.log(msg);
+        res.status(500).json({ message: msg });
+
+    }
 })
 
 router.post('/create', checkAuthenticated, async (req, res) => {
@@ -28,20 +102,28 @@ router.post('/create', checkAuthenticated, async (req, res) => {
 
         const title = req.body.title;
         const trainingFrequency = req.body.trainingFrequency;
-        const trainingWeeks = req.body.trainingWeeks;
+        const trainingPlanWeeks = req.body.trainingWeeks;
         const trainingPhase = req.body.trainingPhase;
         const weightRecommandation = req.body.weightPlaceholders;
+
+        const trainingWeeks = createNewTrainingPlanWithPlaceholders(
+            parseInt(trainingPlanWeeks),
+            parseInt(trainingFrequency)
+          );
 
         // Erstelle einen neuen TrainingBlock
         const newTrainingBlock = new TrainingBlock({
             title: title,
             trainingFrequency: trainingFrequency,
-            trainingWeeks: trainingWeeks,
             trainingPhase: trainingPhase,
             weightRecommandationBase: weightRecommandation,
+            lastUpdated: new Date(),
+            trainingWeeks: trainingWeeks,
         });
 
         user.customPlans.push(newTrainingBlock);
+        user.customPlans.sort((a : any , b : any) => b.lastUpdated - a.lastUpdated);
+
         await user.save();
 
         console.log('Neuer TrainingBlock erstellt:', newTrainingBlock);
@@ -57,5 +139,24 @@ router.post('/create', checkAuthenticated, async (req, res) => {
         });
     }
 });
+
+function createNewTrainingPlanWithPlaceholders(weeks : number, daysPerWeek : number) {
+    const trainingWeeks = [];
+  
+    for (let weekIndex = 0; weekIndex < weeks; weekIndex++) {
+      const trainingDays = [];
+      for (let dayIndex = 0; dayIndex < daysPerWeek; dayIndex++) {
+        const trainingDay = {
+          exercises: [],
+        };
+        trainingDays.push(trainingDay);
+      }
+      trainingWeeks.push({ trainingDays });
+    }
+  
+    return trainingWeeks;
+  }
+
+
 
 export default router;
--- src/public/ts/training/trainingIndex.ts
+++ src/public/ts/training/trainingIndex.ts
@@ -3,42 +3,328 @@ import { validateInput } from "../utils/validateInput.js";
 import { dispatchChangeEventOnAllInputs } from "../utils/dispatchChangeEventOnAllInputs.js";
 import { ApiData } from "../../../../interfaces/ApiData.js";
 
-let changedData : ApiData = {};
+let changedData: ApiData = {};
 
 initializeExercisePatch((data) => {
     changedData = data; // Aktualisiere changedData im höheren Kontext
     console.log(changedData);
-  });
+});
 
-  const form = document.querySelector("form");
-  const createTrainingPlanBTN = document.getElementById("create-training-plan-btn");
+const form = document.querySelector("form");
+const createTrainingPlanBTN = document.getElementById("create-training-plan-btn");
 
-  dispatchChangeEventOnAllInputs(); // because some inputs are preselected for better user experience
+dispatchChangeEventOnAllInputs(); // because some inputs are preselected for better user experience
 
-  createTrainingPlanBTN?.addEventListener("click", e  => {
+createTrainingPlanBTN?.addEventListener("click", e => {
     e.preventDefault();
     console.log("klick");
     form?.dispatchEvent(new Event("submit"));
-  })
+})
 
-  form?.addEventListener("submit", e => {
-      e.preventDefault();
+form?.addEventListener("submit", e => {
+    e.preventDefault();
 
-      if (validateInput(form)) {
-            // prevent default behavious send only changed data
-            sendChangedData("training/create", "POST", changedData);
+    if (validateInput(form)) {
+        // prevent default behavious send only changed data
+        sendChangedData("training/create", "POST", changedData);
 
-            hideCreateTrainingModal();
+        hideCreateTrainingModal();
 
-      } else {
+    } else {
         console.log("nicht alles ausgefüllt mashalla");
-      }
-  })
+    }
+})
 
-  function hideCreateTrainingModal() {
+function hideCreateTrainingModal() {
     //@ts-ignore
     const myModal = new Modal(document.getElementById('exampleModal')!);
     // Verstecke das Modal
     myModal.hide();
-  }
-  
+}
+
+
+
+
+const trainingCards = document.querySelectorAll(".training-card");
+
+let lastSelectedPlanIndex: number;
+
+trainingCards.forEach((card, index) => {
+    card.addEventListener("click", e => {
+        e.preventDefault();
+        e.stopPropagation();
+
+        lastSelectedPlanIndex = index;
+
+        handlePreSelect(card);
+
+    })
+})
+
+function handlePreSelect(card: Element) {
+    trainingCards.forEach(currentCard => {
+        if (card !== currentCard) {
+            currentCard.classList.remove("active")
+        } else {
+            currentCard.classList.add("active");
+            showMoreOptionsContainer();
+        }
+    })
+}
+
+const contentBody = document.getElementById("contentBody");
+contentBody?.addEventListener("click", e => {
+    e.preventDefault();
+
+    deSelectallCards();
+})
+
+function deSelectallCards() {
+    trainingCards.forEach(currentCard => {
+        currentCard.classList.remove("active")
+    })
+    showRegularCreateButton();
+}
+
+
+function showMoreOptionsContainer() {
+    toggleVisibility("optionsButtonContainer", true);
+    toggleVisibility("showCreatePlanModal", false);
+}
+
+function showRegularCreateButton() {
+    toggleVisibility("optionsButtonContainer", false);
+    toggleVisibility("showCreatePlanModal", true);
+}
+
+function toggleVisibility(elementId: string, show: boolean) {
+    const element = document.getElementById(elementId);
+
+    if (element) {
+        if (show) {
+            element.classList.remove("d-none");
+            element.classList.add("d-flex");
+        } else {
+            element.classList.remove("d-flex");
+            element.classList.add("d-none");
+        }
+    }
+}
+
+const deletePlanButton = document.getElementById("delete-plan-button");
+console.log(deletePlanButton);
+
+deletePlanButton?.addEventListener("click", async (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    try {
+        const response = await fetch(`/training/delete/${lastSelectedPlanIndex}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+
+        if (response.ok) {
+            window.location.href = "/training";
+            // Hier kannst du zusätzliche Aktionen nach dem erfolgreichen Löschen durchführen
+        } else {
+            console.error("Fehler beim Löschen des TrainingPlans");
+            // Hier kannst du auf einen Fehlerfall reagieren
+        }
+    } catch (error) {
+        console.error("Fehler beim Fetchen oder Verarbeiten der DELETE-Anfrage", error);
+        // Hier kannst du auf einen Fehlerfall reagieren
+    }
+});
+
+const editPlanButton = document.getElementById("edit-plan-button");
+editPlanButton?.addEventListener("click", async (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    try {
+        const response = await fetch(`training/planInfo/${lastSelectedPlanIndex}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            console.log("Empfangene Daten:", data);
+
+            const modal = createEditModal(data);
+
+
+
+
+
+        } else {
+            console.error("Fehler beim Abrufen der Daten");
+        }
+    } catch (error) {
+        console.error("Fehler beim Fetchen oder Verarbeiten der Anfrage", error);
+    }
+
+})
+
+function createEditModal( data : any) {
+    // Erstelle ein neues Modal-Element
+    const modal = document.createElement("div");
+    modal.className = "modal fade";
+    modal.id = "editModal";
+    modal.tabIndex = -1;
+    modal.role = "dialog";
+    modal.setAttribute("aria-labelledby", "editModalLabel");
+    modal.setAttribute("aria-hidden", "true");
+
+    // Füge den Modal-HTML-Code hinzu
+    modal.innerHTML = `
+        <div class="modal-dialog" role="document">
+        <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Plan bearbeiten</h5>
+          <button id="close-edit-modal" type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+
+        <div class="modal-body">
+                <div class="row no-gutters">
+                  <div class="col-md-12">
+                    <div class="card-body">
+                      <form id="edit-training-form">
+                          <div class="form-group">
+                            <label for="training_title">Titel</label>
+                            <input type="text" class="form-control" value="${data.title}" id="training_title" name="title" required>
+                          </div>
+
+                          <div class="form-group">
+                            <label for="training_frequency">Trainings /
+                                Woche</label>
+                            <select name="trainingFrequency" id="training_frequency" class="form-control" required>
+                                <option value="3" ${ data.trainingFrequency === 3 ? "selected" : ""}>3</option>
+                                <option value="4" ${ data.trainingFrequency === 4 ? "selected" : ""}>4</option>
+                                <option value="5" ${ data.trainingFrequency === 5 ? "selected" : ""}>5</option>
+                                <option value="6" ${ data.trainingFrequency === 6 ? "selected" : ""}>6</option>
+                            </select>
+                          </div>
+
+                          <div class="form-group">
+                            <label for="training_weeks">Blocklänge
+                                (Wochen)</label>
+                            <select name="trainingWeeks" id="training_weeks" class="form-control" required>
+                                <option value="" disabled ${ data.trainingWeeks.length === 0 ? "selected" : ""}></option>
+                                <option value="3" ${ data.trainingWeeks.length === 3 ? "selected" : ""}>3</option>
+                                <option value="4" ${ data.trainingWeeks.length === 4 ? "selected" : ""}>4</option>
+                                <option value="5" ${ data.trainingWeeks.length === 5 ? "selected" : ""}>5</option>
+                                <option value="6" ${ data.trainingWeeks.length === 6 ? "selected" : ""}>6</option>
+                            </select>
+                          </div>
+
+                          <div class="form-group">
+                            <label for="trainingPhaseSelector">Trainingsphase</label>
+                                <select name="trainingPhase" id="trainingPhaseSelector" class="form-control" required>
+                                    <option value="hypertrophie" ${ data.trainingPhase === "hypertrophie" ? "selected" : ""}>Hypertrophie</option>
+                                    <option value="kraft" ${ data.trainingPhase === "kraft" ? "selected" : ""}>Kraft</option>
+                                </select>
+                          </div>
+
+                          <div class="form-group">
+                            <label for="weightPlaceholders">Gewichtsempfehlungen</label>
+                            <select name="weightPlaceholders" class="form-control" required>
+                                <option value="max" ${ data.weightRecommandationBase === "max" ? "selected" : ""}>basierend auf Rechenmax</option>
+                                <option value="lastWeek" ${ data.weightRecommandationBase === "lastWeek" ? "selected" : ""}>basierend auf letzter Woche</option>
+                                <option value="off">keine Gewichtsempfehlungen</option>
+                            </select>
+                          </div>
+        
+                        </form>
+                        </nav>
+                    </div>
+                  </div>
+                </div>
+        </div>
+
+        <div class="modal-footer">
+          <button id="close-edit-modal-2" type="button" class="btn btn-light" data-dismiss="modal">Close</button>
+          <button id="edit-training-plan-btn" type="button" class="btn  btn-info">Edit</button>
+        </div>
+      </div>
+        </div>
+    `;
+
+    // Füge das Modal zum DOM hinzu
+    document.body.appendChild(modal);
+
+    setUpEditModalFunctionality();
+
+    //@ts-ignore
+    const bootstrapModal = new bootstrap.Modal(modal);
+    bootstrapModal.show();
+
+    return modal;
+}
+
+
+// maybe make this generic
+function setUpEditModalFunctionality() {
+    const modal =  document.getElementById("editModal");
+
+    const editTrainingPlanButton = document.getElementById("edit-training-plan-btn");
+    const editTrainingForm = document.getElementById("edit-training-form") as HTMLFormElement;
+
+    // dispatch event on submit when clicked
+    editTrainingPlanButton?.addEventListener("click", e => {
+        e.preventDefault();
+        e.stopPropagation();
+
+        editTrainingForm?.dispatchEvent(new Event("submit"));
+    })
+
+    editTrainingForm?.addEventListener("submit", async (e) => {
+        
+        const formData = new FormData(editTrainingForm);
+        const patchUrl = `training/plan/${lastSelectedPlanIndex}`;
+
+        try {
+            const response = await fetch(patchUrl, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(Object.fromEntries(formData)),
+            });
+
+            if (response.ok) {
+                // Erfolgreich aktualisiert
+                window.location.href = "/training";
+
+            } else {
+                // Fehler beim Aktualisieren des Plans
+                console.error("Fehler beim Aktualisieren des Plans");
+            }
+        } catch (error) {
+            console.error("Fehler beim Fetchen oder Verarbeiten der Anfrage", error);
+        }
+
+    })
+
+    const closeEditModal = document.getElementById("close-edit-modal");
+    const closeEditModa2 = document.getElementById("close-edit-modal-2");
+
+    closeEditModal?.addEventListener("click", () => {
+        modal?.remove();
+        console.log("entfernt");
+    })
+
+    closeEditModa2?.addEventListener("click", () => {
+        modal?.remove();
+        console.log("entfernt");
+    })
+
+
+}
