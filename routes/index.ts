d5998df0edecedf4ca1258a394659d82d5ae0f57
Mathis Arends <mathisarends27@gmail.com>
2024-02-23 11:53:20 +0100

    Added profile controller and fixed mobile design for profile

--- routes/index.ts
+++ routes/index.ts
@@ -1,26 +1,10 @@
 import express from "express";
 import { Request, Response } from "express";
 import User from "../src/models/user.model.js";
-import { CustomRequest } from "../interfaces/CustomRequest.js";
-import { checkNotAuthenticated, checkAuthenticated } from "../middleware/authMiddleware.js";
-
+import { handleIndexRoute } from "../controller/indexController.js";
 const router = express.Router();
 
 // Routes
-router.get("/", async (req: Request, res: Response) => {
-    try {
-              // @ts-ignore
-      const user = await User.findById(req.user?._id);
-
-        res.render("index", {
-            defaultLayout: true,
-            carousel: true,
-            user: user
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("Internal Server Error");
-    }
-});
+router.get("/", (req, res) => handleIndexRoute(req, res));
 
 export default router;
