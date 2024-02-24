305d512eefb5587b995f2502dbbfe54e0842c255
Mathis Arends <mathisarends27@gmail.com>
2024-02-24 06:19:30 +0100

    Added refactor for profile page

--- routes/profile.ts
+++ routes/profile.ts
@@ -1,59 +1,10 @@
 import express from "express";
 import { checkAuthenticated } from "../middleware/authMiddleware.js";
-import { formatDateMonthAndYear } from "../src/public/generic/formatDate.js";
-import { ApiData } from "../interfaces/ApiData.js";
+import { getProfilePage, patchProfilePage } from "../controller/profileController.js";
 
 const router = express.Router();
 
-router.get("/", checkAuthenticated, (req, res) => {
-
-    const user = res.locals.user;
-    const username = user.name;
-    const email = user.email;
-    const memberSince = formatDateMonthAndYear(user.createdAt);
-    const description = user.description;
-
-    res.render("profile", {
-        defaultLayout: true,
-        carousel: false,
-        username,
-        email,
-        description,
-        memberSince
-    })
-})
-
-router.patch("/", checkAuthenticated, async (req, res) => {
-    try {
-        const user = res.locals.user;
-        const changedData: ApiData = req.body;
-
-        const fieldMappings : Record<string, string> = {
-            "userName": "name",
-            "userMail": "email",
-            "userDescription": "description",
-        };
-
-        Object.entries(changedData).forEach(([fieldName, fieldValue]) => {
-            const userField = fieldMappings[fieldName];
-            if (userField) {
-                user[userField] = fieldValue;
-            }
-        });
-
-        await user.save();
-
-        res.status(200).json({
-            message: "Benutzerdaten erfolgreich aktualisiert."
-        });
-
-    } catch (error) {
-        const errMessage = "Es ist ein Fehler beim Patchen der Benutzerdaten aufgetreten " +  error;
-        console.log(errMessage);        
-        res.status(500).json({
-            message: errMessage
-        });
-    }
-});
+router.get("/", checkAuthenticated, (req, res) => getProfilePage(req, res))
+router.patch("/", checkAuthenticated, (req, res) => patchProfilePage(req, res));
 
 export default router;
